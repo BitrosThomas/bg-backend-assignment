eac24283d9d53cc0b4a33c2cb89b3529b4ef9232
Thomas <bitros.thomas@gmail.com>
2019-01-18 14:09:11 +0200

    -Added double search with title and region.

---
+++ src/main/java/com/thomas/blueground/forms/SearchForm.java
@@ -0,0 +1,23 @@
+package com.thomas.blueground.forms;
+
+public class SearchForm {
+
+    private String title;
+    private String region;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+}
